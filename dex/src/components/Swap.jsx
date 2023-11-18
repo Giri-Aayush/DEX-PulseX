e5bf0267741fb438568049dd2481569215a3a081
Giri-Aayush <aayushgiri1234@gmail.com>
2023-11-18 07:36:17 +0530

    add token choices

--- dex/src/components/Swap.jsx
+++ dex/src/components/Swap.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from 'react'
 import {Input, Popover, Radio, Modal, message} from "antd";
 import {DownOutlined, ArrowDownOutlined, SettingOutlined} from "@ant-design/icons";
+import tokenList from "../tokenList.json";
 
 function Swap() {
 
   const [slippage, setSlippage] = useState(2.5);
   const [tokenOneAmount, setTokenOneAmount] = useState(null);
   const [tokenTwoAmount, setTokenTwoAmount] = useState(null);
+  const [tokenOne, setTokenOne]  = useState(tokenList[0]);
+  const [tokenTwo, setTokenTwo] = useState(tokenList[1]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [changeToken, setChangeToken] = useState(1);
 
   function handleSlippageChange(e){
     setSlippage(e.target.value);
@@ -14,6 +20,29 @@ function Swap() {
 
   function changeAmount(e){
     setTokenOneAmount(e.target.value);
+  }
+
+  function switchTokens(){
+    const one = tokenOne;
+    const two = tokenTwo;
+    setTokenOne(two);
+    setTokenTwo(one);
+  }
+
+  function openModal(asset){
+    
+    setChangeToken(asset);
+    setIsOpen(true);
+    
+  }
+
+  function modifyToken(index){
+    if(changeToken === 1){
+      setTokenOne(tokenList[index]);
+    }else{
+      setTokenTwo(tokenList[index]);
+    }
+    setIsOpen(false);
   }
 
   const settings = (
@@ -30,6 +59,31 @@ function Swap() {
   )
 
   return (
+    <>
+    <Modal
+        open={isOpen}
+        footer={null}
+        onCancel={() => setIsOpen(false)}
+        title="Select a token"
+      >
+        <div className="modalContent">
+          {tokenList?.map((e, i) => {
+            return (
+              <div
+                className="tokenChoice"
+                key={i}
+                onClick={() => modifyToken(i)}
+              >
+                <img src={e.img} alt={e.ticker} className="tokenLogo" />
+                <div className="tokenChoiceNames">
+                  <div className="tokenName">{e.name}</div>
+                  <div className="tokenTicker">{e.ticker}</div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </Modal>
     <div className='tradeBox'>
       <div className='tradeBoxHeader'>
         <h4>Swap</h4>
@@ -45,8 +99,23 @@ function Swap() {
       <div className='inputs'>
         <Input placeholder='0' value={tokenOneAmount} onChange={changeAmount} />
         <Input placeholder='0' value={tokenTwoAmount} disabled={true}/>
+        <div className='switchButton' onClick={switchTokens}>
+          <ArrowDownOutlined className='switchArrow'/>
+        </div>
+        <div className='assetOne' onClick={() => openModal(1)}>
+          <img src={tokenOne.img} alt='assetOneLogo' className='assetLogo'/>
+          {tokenOne.ticker}
+          <DownOutlined/>
+        </div>
+        <div className='assetTwo' onClick={() => openModal(2)}>
+        <img src={tokenTwo.img} alt='assetTwoLogo' className='assetLogo'/>
+          {tokenTwo.ticker}
+          <DownOutlined/>
+        </div>
       </div>
+      <div className='swapButton' disabled={!tokenOneAmount}>Swap</div>
     </div>
+    </>
   )
 }
 
