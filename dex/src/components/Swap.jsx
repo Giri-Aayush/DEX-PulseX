1ea3d5582fbf877ffe8e9adeeb3f86d5f11175d1
Giri-Aayush <aayushgiri1234@gmail.com>
2023-11-20 17:04:40 +0530

    get token prices frontend

--- dex/src/components/Swap.jsx
+++ dex/src/components/Swap.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import {Input, Popover, Radio, Modal, message} from "antd";
-import {DownOutlined, ArrowDownOutlined, SettingOutlined} from "@ant-design/icons";
+import { Input, Popover, Radio, Modal, message } from "antd";
+import { DownOutlined, ArrowDownOutlined, SettingOutlined } from "@ant-design/icons";
 import tokenList from "../tokenList.json";
 import axios from 'axios';
 
@@ -9,75 +9,88 @@ function Swap() {
   const [slippage, setSlippage] = useState(2.5);
   const [tokenOneAmount, setTokenOneAmount] = useState(null);
   const [tokenTwoAmount, setTokenTwoAmount] = useState(null);
-  const [tokenOne, setTokenOne]  = useState(tokenList[0]);
+  const [tokenOne, setTokenOne] = useState(tokenList[0]);
   const [tokenTwo, setTokenTwo] = useState(tokenList[1]);
   const [isOpen, setIsOpen] = useState(false);
 
   const [changeToken, setChangeToken] = useState(1);
   const [prices, setPrices] = useState(null);
 
-  function handleSlippageChange(e){
+  function handleSlippageChange(e) {
     setSlippage(e.target.value);
   }
 
-  function changeAmount(e){
+  function changeAmount(e) {
     setTokenOneAmount(e.target.value);
+    if (e.target.value && prices) {
+      setTokenTwoAmount((e.target.value * prices.ratio).toFixed(3))
+    } else {
+      setTokenTwoAmount(null);
+    }
   }
 
-  function switchTokens(){
+  function switchTokens() {
+    setPrices(null);
+    setTokenOneAmount(null);
+    setTokenTwoAmount(null);
     const one = tokenOne;
     const two = tokenTwo;
     setTokenOne(two);
     setTokenTwo(one);
+    fetchPrices(two.address, one.address);
   }
 
-  function openModal(asset){
-    
+  function openModal(asset) {
+
     setChangeToken(asset);
     setIsOpen(true);
-    
+
   }
 
-  function modifyToken(index){
-    if(changeToken === 1){
+  function modifyToken(index) {
+    setPrices(null);
+    setTokenOneAmount(null);
+    setTokenTwoAmount(null);
+    if (changeToken === 1) {
       setTokenOne(tokenList[index]);
-    }else{
+      fetchPrices(tokenList[index].address, tokenTwo.address);
+    } else {
       setTokenTwo(tokenList[index]);
+      fetchPrices(tokenOne.address, tokenList[index].address);
     }
     setIsOpen(false);
   }
 
-  async function fetchPrices(one, two){
+  async function fetchPrices(one, two) {
     const res = await axios.get(`http://localhost:3001/tokenPrice`, {
       params: {
         addressOne: one,
         addressTwo: two
       }
     })
-    console.log(res);
     setPrices(res.data);
   }
 
   useEffect(() => {
-    fetchPrices(tokenList[0].address,tokenList[1].address)
-  },[])
+    fetchPrices(tokenList[0].address, tokenList[1].address)
+  }, [])
 
   const settings = (
     <>
-    <div>Slippage Tolerance</div>
-    <div>
-      <Radio.Group value={slippage} onChange={handleSlippageChange}>
-        <Radio.Button value={0.5}>0.5%</Radio.Button>
-        <Radio.Button value={2.5}>2.5%</Radio.Button>
-        <Radio.Button value={5.0}>5.0%</Radio.Button>
-      </Radio.Group>
-    </div>
+      <div>Slippage Tolerance</div>
+      <div>
+        <Radio.Group value={slippage} onChange={handleSlippageChange}>
+          <Radio.Button value={0.5}>0.5%</Radio.Button>
+          <Radio.Button value={2.5}>2.5%</Radio.Button>
+          <Radio.Button value={5.0}>5.0%</Radio.Button>
+        </Radio.Group>
+      </div>
     </>
   )
 
   return (
     <>
-    <Modal
+      <Modal
         open={isOpen}
         footer={null}
         onCancel={() => setIsOpen(false)}
@@ -101,37 +114,37 @@ function Swap() {
           })}
         </div>
       </Modal>
-    <div className='tradeBox'>
-      <div className='tradeBoxHeader'>
-        <h4>Swap</h4>
-        <Popover
-        content={settings}
-        title='Settings'
-        trigger="click"
-        placement='bottomRight'
-        >
-          <SettingOutlined className='cog'/>
-        </Popover>
+      <div className='tradeBox'>
+        <div className='tradeBoxHeader'>
+          <h4>Swap</h4>
+          <Popover
+            content={settings}
+            title='Settings'
+            trigger="click"
+            placement='bottomRight'
+          >
+            <SettingOutlined className='cog' />
+          </Popover>
+        </div>
+        <div className='inputs'>
+          <Input placeholder='0' value={tokenOneAmount} onChange={changeAmount} disabled={!prices} />
+          <Input placeholder='0' value={tokenTwoAmount} disabled={true} />
+          <div className='switchButton' onClick={switchTokens}>
+            <ArrowDownOutlined className='switchArrow' />
+          </div>
+          <div className='assetOne' onClick={() => openModal(1)}>
+            <img src={tokenOne.img} alt='assetOneLogo' className='assetLogo' />
+            {tokenOne.ticker}
+            <DownOutlined />
+          </div>
+          <div className='assetTwo' onClick={() => openModal(2)}>
+            <img src={tokenTwo.img} alt='assetTwoLogo' className='assetLogo' />
+            {tokenTwo.ticker}
+            <DownOutlined />
+          </div>
+        </div>
+        <div className='swapButton' disabled={!tokenOneAmount}>Swap</div>
       </div>
-      <div className='inputs'>
-        <Input placeholder='0' value={tokenOneAmount} onChange={changeAmount} />
-        <Input placeholder='0' value={tokenTwoAmount} disabled={true}/>
-        <div className='switchButton' onClick={switchTokens}>
-          <ArrowDownOutlined className='switchArrow'/>
-        </div>
-        <div className='assetOne' onClick={() => openModal(1)}>
-          <img src={tokenOne.img} alt='assetOneLogo' className='assetLogo'/>
-          {tokenOne.ticker}
-          <DownOutlined/>
-        </div>
-        <div className='assetTwo' onClick={() => openModal(2)}>
-        <img src={tokenTwo.img} alt='assetTwoLogo' className='assetLogo'/>
-          {tokenTwo.ticker}
-          <DownOutlined/>
-        </div>
-      </div>
-      <div className='swapButton' disabled={!tokenOneAmount}>Swap</div>
-    </div>
     </>
   )
 }
